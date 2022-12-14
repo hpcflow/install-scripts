955f6bf68fffe127e6962d42f019fd5517078b9c
cjfullerton <chris.j.fullerton@googlemail.com>
2022-12-14 16:14:16 +0000

    feat: suppress curl and unzip output

--- test-install-complete.sh
+++ test-install-complete.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 linux_install_dir="~/.local/share/hpcflow"
-macOS_install_dir="~/Library/Application Support/hpcflow"
+macOS_install_dir="~/Library/Application\ Support/hpcflow"
 windows_install_dir="%USERPROFILE%\AppData\Local\hpcflow"
 app_name="hpcflow"
 base_link="https://github.com/hpcflow/hpcflow-new/releases/download"
@@ -13,15 +13,21 @@ windows_ending="windows-dir.zip"
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
         artifact_name="${app_name}-${latest_version}-${linux_ending}"
         download_link="${base_link}/${latest_version}/${artifact_name}"
-        echo "linux"
-        curl $download_link -O -L
-	unzip $artifact_name
+        echo "Downloading hpcflow ..."
+        curl -s $download_link -O -L
+        echo "Installing hpcflow ..."
+	unzip -qq $artifact_name
+        mkdir $linux_install_dir
+        mv dist/onedir/$artifact_name $linux_install_dir
 elif [[ "$OSTYPE" == "darwin"* ]]; then
         artifact_name="${app_name}-${latest_version}-${macOS_ending}"
         download_link="${base_link}/${latest_version}/${artifact_name}"
-        echo "macOS"
-        curl $download_link -O -L
-        unzip $artifact_name
+        echo "Downloading hpcflow ..."
+        curl -s $download_link -O -L
+        echo "Installing hpcflow ..."
+        unzip -qq $artifact_name
+        mkdir $macOS_install_dir
+        mv dist/onedir/$artifact_name $macOS_install_dir
 else
         echo "Operating system ${OSTYPE} not supported."
 fi
