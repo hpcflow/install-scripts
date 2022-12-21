1ce8272b38a453181d11f9bc6ee03d28ba4bdc29
cjfullerton <chris.j.fullerton@googlemail.com>
2022-12-21 10:50:04 +0000

    fix: powershell param cmdlet moved

--- install-hpcflow.ps1
+++ install-hpcflow.ps1
@@ -1,3 +1,5 @@
+param($version="v0.2.0a18", $folder="${env:USERPROFILE}\AppData\Local\${app_name})
+
 $app_name="hpcflow"
 $base_link="https://github.com/hpcflow/hpcflow-new/releases/download"
 
@@ -9,8 +11,6 @@ $latest_version="v0.2.0a18"
 $progress_string_1="Step 1 of 2: Downloading ${app_name} ..."
 $progress_string_2="Step 2 of 2: Installing ${app_name} ..."
 $completion_string_1="Installation of ${app_name} complete."
-
-param($version=$latest_version, $folder=$windows_install_dir)
 
 $artifact_name="${app_name}-${version}-${windows_ending}.zip"
 $folder_name="${app_name}-${version}-${windows_ending}"
