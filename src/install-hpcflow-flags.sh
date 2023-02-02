e11cc85346402e3a552622404d5adac2559c9dde
cjfullerton <chris.j.fullerton@googlemail.com>
2023-02-02 16:12:21 +0000

    fix: command line args now passed to run_main

--- src/install-hpcflow-flags.sh
+++ src/install-hpcflow-flags.sh
@@ -1,72 +1,41 @@
 #!/bin/bash
+run_main() {
 
-app_name="hpcflow"
-base_link="https://github.com/hpcflow/hpcflow-new/releases/download"
+	set_variables
 
-linux_ending_folder="linux-dir"
-macOS_ending_folder="macOS-dir"
-linux_ending_file="linux"
-macOS_ending_file="macOS"
+	set_flags
 
-linux_install_dir=~/.local/share/hpcflow
-macOS_install_dir=~/Library/Application\ Support/hpcflow
+	make_tempdir
 
-#latest_stable_releases="https://raw.githubusercontent.com/hpcflow/hpcflow-new/main/docs/source/released_binaries.yml"
-latest_stable_releases="https://raw.githubusercontent.com/hpcflow/hpcflow-new/dummy-stable/docs/source/released_binaries.yml"
-latest_prelease_releases="https://raw.githubusercontent.com/hpcflow/hpcflow-new/develop/docs/source/released_binaries.yml"
+	# Assign command line variables and flags
+	while [ $# -gt 0 ]; do
 
-progress_string_1="Step 1 of 2: Downloading ${app_name} ..."
-progress_string_2="Step 2 of 2: Installing ${app_name} ..."
-completion_string_1="Installation of ${app_name} complete."
+		if [[ $1 == *"--"* ]]; then
 
-# Flag variables false by default
-prerelease=false
-purge=false
-path=false
+			param="${1/--/}"
 
-# Flag to note if user specified version
-versionspec=false
-# Flag recording if hpcflow symlink folder is on path
-onpath=false
+			if [[ "$param" == "prerelease" ]] || [[ $param == "purge" ]] || [[ $param == "path" ]] || [[ $param == "onefile" ]] || [[ $param == "univlink" ]]; then
+				declare $param=true
+			else
+				declare $param="$2"
+				if [[ "$param" == "version" ]]; then
+					versionspec=true
+				fi
 
-# Make temp diretory and store path in a variable
-TEMPD=$(mktemp -d)
-
-# Exit if temp directory wasn't created successfully
-if [ ! -r "$TEMPD" ]; then
-	>&2 echo "Failed to create temp directory for download"
-	exit 1
-fi
-
-# Assign command line variables and flags
-while [ $# -gt 0 ]; do
-
-    if [[ $1 == *"--"* ]]; then
-
-        param="${1/--/}"
-
-        if [[ "$param" == "prerelease" ]] || [[ $param == "purge" ]] || [[ $param == "path" ]] || [[ $param == "onefile" ]] || [[ $param == "univlink" ]]; then
-            declare $param=true
-        else
-            declare $param="$2"
-			if [[ "$param" == "version" ]]; then
-				versionspec=true
 			fi
 
-        fi
+			echo $1 $2 // Optional to see the parameter:value result
 
-        # echo $1 $2 // Optional to see the parameter:value result
+		fi
 
-    fi
+		shift
 
-    shift
+	done
 
-done
+	# Set OS specific variables
+	if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 
-# Set OS specific variables
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-
-        folder=${folder:-${linux_install_dir}}
+		folder=${folder:-${linux_install_dir}}
 
 		if [[ "$onefile" = true ]]; then
 			app_name_ending=$linux_ending_file
@@ -75,10 +44,10 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 			app_name_ending=$linux_ending_folder
 			file_name_ending="${linux_ending_folder}.zip"
 		fi
-        
-elif [[ "$OSTYPE" == "darwin"* ]]; then
 
-        folder=${folder:-${macOS_install_dir}}
+	elif [[ "$OSTYPE" == "darwin"* ]]; then
+
+		folder=${folder:-${macOS_install_dir}}
 
 		if [[ "$onefile" = true ]]; then
 			app_name_ending=$macOS_ending_file
@@ -88,201 +57,286 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
 			file_name_ending="${macOS_ending_folder}.zip"
 		fi
 
-else
-        echo "Operating system ${OSTYPE} not supported."
+	else
+		echo "Operating system ${OSTYPE} not supported."
 		exit 1
-fi			
+	fi
 
-# If prelease flag is set, default is latest prerelease, otherwise latest stable
-# Both overridden if version specified by user
-if [ "$prerelease" = true ]; then
+	# If prelease flag is set, default is latest prerelease, otherwise latest stable
+	# Both overridden if version specified by user
+	if [ "$prerelease" = true ]; then
 
-	echo "Installing latest prelease version."
-	sleep 0.2
+		echo "Installing latest prelease version."
+		sleep 0.2
 
-	artifact_name=`curl -s $latest_prelease_releases | grep "${file_name_ending}:" | cut -d ":" -f 1`
-	version=`echo $artifact_name | cut -d '-' -f 2`
+		artifact_name=$(curl -s $latest_prelease_releases | grep "${file_name_ending}:" | cut -d ":" -f 1)
+		version=$(echo $artifact_name | cut -d '-' -f 2)
 
-elif  [ "$versionspec" = true ]; then
+	elif [ "$versionspec" = true ]; then
 
-	echo "Installing ${app_name} version ${version}."
-	sleep 0.2
-	version=${version}
-	artifact_name="${app_name}-${version}-${file_name_ending}"
-
-else
-
-	echo "Installing latest stable version"
-	sleep 0.2
-
-	artifact_name=`curl -s $latest_stable_releases | grep "${file_name_ending}:" | cut -d ":" -f 1`
-	version=`echo $artifact_name | cut -d '-' -f 2`
-
-fi
-
-touch "${folder}"/user_versions.txt
-touch "${folder}"/stable_versions.txt
-
-if [ `grep -c "${version}-{file_name_ending}" "${folder}"/user_versions.txt` -ge 1 ] || [ `grep -c "${version}-{file_name_ending}" "${folder}"/stable_versions.txt` -ge 1 ]; then
-
-	echo "${app_name} ${version} already installed on this system... "
-	sleep 0.2
-	echo "Exiting..."
-	exit 1
-
-fi
-
-# Check if folders on path already
-case :$PATH: in
-	*:"${folder}"/links:)	onpath=true ;;
-	*) 
-esac
-
-# Set variables for download and install
-folder_name="${app_name}-${version}-${app_name_ending}"
-download_link="${base_link}/${version}/${artifact_name}"
-
-if [ "$purge" != true ]; then
-
-	echo $progress_string_1
-	echo
-	curl -s --output-dir $TEMPD $download_link -O -L
-	echo $progress_string_2
-	echo
-
-	mkdir -p "${folder}"
-	mkdir -p "${folder}/links"
-
-	if [ "$onefile" != true ]; then
-
-		unzip -qq $TEMPD/$artifact_name -d $TEMPD
-		chmod -R u+rw $TEMPD/dist/onedir/$folder_name
-		mv -n $TEMPD/dist/onedir/$folder_name "${folder}"	
-
-	elif [ "$onefile" == true ]; then
-
-		chmod u+rwx $TEMPD/$artifact_name
-		mv -n $TEMPD/$artifact_name "${folder}"
+		echo "Installing ${app_name} version ${version}."
+		sleep 0.2
+		version=${version}
+		artifact_name="${app_name}-${version}-${file_name_ending}"
 
 	else
 
-        echo "Unexpected error."
+		echo "Installing latest stable version"
+		sleep 0.2
+
+		artifact_name=$(curl -s $latest_stable_releases | grep "${file_name_ending}:" | cut -d ":" -f 1)
+		version=$(echo $artifact_name | cut -d '-' -f 2)
+
+	fi
+
+	touch "${folder}"/user_versions.txt
+	touch "${folder}"/stable_versions.txt
+
+	if [ $(grep -c "${version}-{app_name_ending}" "${folder}"/user_versions.txt) -ge 1 ] || [ $(grep -c "${version}-{app_name_ending}" "${folder}"/stable_versions.txt) -ge 1 ]; then
+
+		echo "${app_name} ${version} already installed on this system... "
+		sleep 0.2
+		echo "Exiting..."
 		exit 1
 
 	fi
 
-	# Create symlinks and clear up older versions
-	if [[ "$prerelease" != true ]] && [[ "$versionspec" != true ]]; then
+	# Check if folders on path already
+	case :$PATH: in
+	*:"${folder}"/links:) onpath=true ;;
+	*) ;;
+	esac
 
-		# Default - pre-release or specific version not specified
-		#
-		# Create generic app_name sym link to latest release along with specific version numbered link
+	# Set variables for download and install
+	folder_name="${app_name}-${version}-${app_name_ending}"
+	download_link="${base_link}/${version}/${artifact_name}"
+
+	if [ "$purge" != true ]; then
+
+		echo $progress_string_1
+		echo
+		curl -s --output-dir $TEMPD $download_link -O -L
+		echo $progress_string_2
+		echo
+
+		mkdir -p "${folder}"
+		mkdir -p "${folder}/links"
 
 		if [ "$onefile" != true ]; then
 
-			ln -sf "${folder}/${folder_name}/${folder_name}" "${folder}/links/${folder_name}"
+			unzip -qq $TEMPD/$artifact_name -d $TEMPD
+			chmod -R u+rw $TEMPD/dist/onedir/$folder_name
+			mv -n $TEMPD/dist/onedir/$folder_name "${folder}"
 
-			echo "-not -name ${folder_name}" >> "${folder}"/stable_versions.txt
+		elif [ "$onefile" == true ]; then
 
-			# Record sym link names to inform user
-			symstring="${folder_name}"
+			chmod u+rwx $TEMPD/$artifact_name
+			mv -n $TEMPD/$artifact_name "${folder}"
 
-			if [ "$univlink" == true ]; then
+		else
 
-				ln -sf "${folder}/${folder_name}/${folder_name}" "${folder}/links/${app_name}"
+			echo "Unexpected error."
+			exit 1
+
+		fi
+
+		# Create symlinks and clear up older versions
+		if [[ "$prerelease" != true ]] && [[ "$versionspec" != true ]]; then
+
+			# Default - pre-release or specific version not specified
+			#
+			# Create generic app_name sym link to latest release along with specific version numbered link
+
+			if [ "$onefile" != true ]; then
+
+				ln -sf "${folder}/${folder_name}/${folder_name}" "${folder}/links/${folder_name}"
+
+				echo "-not -name ${folder_name}" >>"${folder}"/stable_versions.txt
+
+				# Record sym link names to inform user
+				symstring="${folder_name}"
+
+				if [ "$univlink" == true ]; then
+
+					ln -sf "${folder}/${folder_name}/${folder_name}" "${folder}/links/${app_name}"
+					symstring="${app_name} or ${folder_name}"
+
+				fi
+
+			elif [ "$onefile" == true ]; then
+
+				ln -sf "${folder}/${artifact_name}" "${folder}/links/${app_name}"
+				ln -sf "${folder}/${artifact_name}" "${folder}/links/${artifact_name_name}"
+
+				echo "-not -name ${folder_name}" >>"${folder}"/stable_versions.txt
+
+				# Record sym link names to inform user
 				symstring="${app_name} or ${folder_name}"
+
+			else
+
+				echo "Unexpected error."
+				exit 1
 
 			fi
 
-		elif [ "$onefile" == true ]; then
+			tail -n3 "${folder}"/stable_versions.txt >>"${folder}"/to_keep.txt
+			mv "${folder}"/to_keep.txt "${folder}"/stable_versions.txt
 
-			ln -sf "${folder}/${artifact_name}" "${folder}/links/${app_name}"
-			ln -sf "${folder}/${artifact_name}" "${folder}/links/${artifact_name_name}"
-
-			echo "-not -name ${folder_name}" >> "${folder}"/stable_versions.txt
-
-			# Record sym link names to inform user
-			symstring="${app_name} or ${folder_name}"
+			# Delete symlinks and folders not stored in stable_versions.txt and user_versions.txt
+			find "${folder}"/links/"${app_name}"-v* $(cat "${folder}"/stable_versions.txt 2>/dev/null) $(cat "${folder}"/user_versions.txt 2>/dev/null) -delete
+			find "${folder}"/"${app_name}"-v* $(cat "${folder}"/stable_versions.txt 2>/dev/null) $(cat "${folder}"/user_versions.txt 2>/dev/null) -delete
 
 		else
 
-        	echo "Unexpected error."
-			exit 1
+			if [ "$onefile" != true ]; then
+				ln -sf "${folder}/${folder_name}/${folder_name}" "${folder}/links/${folder_name}"
+
+				echo "-not -name ${folder_name}" >>"${folder}"/user_versions.txt
+
+				# Record sym link names to inform user
+
+				symstring=${folder_name}
+			elif [ "$onefile" == true ]; then
+				ln -sf "${folder}/${folder_name}/${folder_name}" "${folder}/links/${folder_name}"
+
+				echo "-not -name ${folder_name}" >>"${folder}"/user_versions.txt
+
+				# Record sym link names to inform user
+
+				symstring=${folder_name}
+			else
+				echo "Unexpected error."
+				exit 1
+			fi
 
 		fi
 
-		tail -n3 "${folder}"/stable_versions.txt >> "${folder}"/to_keep.txt
-		mv "${folder}"/to_keep.txt "${folder}"/stable_versions.txt
+		if [ "$path" = true ]; then
 
-		# Delete symlinks and folders not stored in stable_versions.txt and user_versions.txt
-		find "${folder}"/links/"${app_name}"-v* `cat "${folder}"/stable_versions.txt 2> /dev/null` `cat "${folder}"/user_versions.txt 2> /dev/null` -delete
-		find "${folder}"/"${app_name}"-v* `cat "${folder}"/stable_versions.txt 2> /dev/null` `cat "${folder}"/user_versions.txt 2> /dev/null` -delete
+			# Check which files exist
+			if [ $(test -f ~/.zshrc) ] && [[ "$onpath" = false ]]; then
+				echo "Updating ~/.zshrc..."
+				echo "export PATH=\"\$PATH:"${folder}"/links\"" >>~/.zshrc
+			fi
 
-	else
-
-		if [ "$onefile" != true ]; then
-			ln -sf "${folder}/${folder_name}/${folder_name}" "${folder}/links/${folder_name}"
-
-			echo "-not -name ${folder_name}" >> "${folder}"/user_versions.txt
-
-			# Record sym link names to inform user
-
-			symstring=${folder_name}
-		elif [ "$onefile" == true ]; then
-			ln -sf "${folder}/${folder_name}/${folder_name}" "${folder}/links/${folder_name}"
-
-			echo "-not -name ${folder_name}" >> "${folder}"/user_versions.txt
-
-			# Record sym link names to inform user
-
-			symstring=${folder_name}
-		else
-        	echo "Unexpected error."
-			exit 1
+			if [ $(test -f ~/.bashrc) ] && [[ "$onpath" = false ]]; then
+				echo "Updating ~/.bashrc..."
+				echo "export PATH=\"\$PATH:"${folder}"/links\"" >>~/.bashhrc
+			fi
 		fi
 
-	fi
-
-	if [ "$path" = true ]; then
-
-		# Check which files exist
-		if [ `test -f ~/.zshrc` ] && [[ "$onpath" = false ]]; then
-			echo "Updating ~/.zshrc..."
-			echo "export PATH=\"\$PATH:"${folder}"/links\"" >> ~/.zshrc
+		echo $completion_string_1
+		sleep 0.2
+		if [ "$path" != true ] && [ "$onpath" != true ]; then
+			echo
+			echo
+			echo "Add "${app_name}" to path by adding the following line to ~/.bashrc or ~/.zshrc:"
+			echo "export PATH=\"\$PATH:"${folder}"/links\""
 		fi
-
-		if [ `test -f ~/.bashrc` ] && [[ "$onpath" = false ]]; then
-			echo "Updating ~/.bashrc...";
-			echo "export PATH=\"\$PATH:"${folder}"/links\"" >> ~/.bashhrc
-		fi
-	fi
-
-	echo $completion_string_1
-	sleep 0.2
-	if [ "$path" != true ] && [ "$onpath" != true ]; then
 		echo
 		echo
-		echo "Add "${app_name}" to path by adding the following line to ~/.bashrc or ~/.zshrc:"
-		echo "export PATH=\"\$PATH:"${folder}"/links\""
+		echo "Re-open terminal and then type ${symstring} to get started."
+
+	elif [ "$purge" = true ]; then
+
+		echo "Purging local install of "${app_name}"..."
+		sleep 0.2
+		echo "I say we take off and nuke the entire site from orbit. It's the only way to be sure."
+		# echo "Deleting "${app_name}" folder "${folder}"..."
+		# sleep 0.2
+		#rm -r  "${folder}"
+		# echo "Removing "${app_name}" folder "$folder}" from path..."
+		# sleep 0.2
+		# COMMAND GOES HERE
+
 	fi
-	echo
-	echo
-	echo "Re-open terminal and then type ${symstring} to get started."
 
-elif [ "$purge" = true ]; then
-	
-	echo "Purging local install of "${app_name}"..."
-	sleep 0.2
-	echo "I say we take off and nuke the entire site from orbit. It's the only way to be sure."
-	# echo "Deleting "${app_name}" folder "${folder}"..."
-	# sleep 0.2
-	#rm -r  "${folder}"
-	# echo "Removing "${app_name}" folder "$folder}" from path..."
-	# sleep 0.2
-	# COMMAND GOES HERE
+	# Make sure temp directory is deleted on exit
+	trap "exit 1" HUP INT PIPE QUIT TERM
+	trap 'rm -rf $TEMPD' EXIT
 
+}
+
+set_variables() {
+
+	app_name="hpcflow"
+	base_link="https://github.com/hpcflow/hpcflow-new/releases/download"
+
+	linux_ending_folder="linux-dir"
+	macOS_ending_folder="macOS-dir"
+	linux_ending_file="linux"
+	macOS_ending_file="macOS"
+
+	linux_install_dir=~/.local/share/hpcflow
+	macOS_install_dir=~/Library/Application\ Support/hpcflow
+
+	#latest_stable_releases="https://raw.githubusercontent.com/hpcflow/hpcflow-new/main/docs/source/released_binaries.yml"
+	latest_stable_releases="https://raw.githubusercontent.com/hpcflow/hpcflow-new/dummy-stable/docs/source/released_binaries.yml"
+	latest_prelease_releases="https://raw.githubusercontent.com/hpcflow/hpcflow-new/develop/docs/source/released_binaries.yml"
+
+	progress_string_1="Step 1 of 2: Downloading ${app_name} ..."
+	progress_string_2="Step 2 of 2: Installing ${app_name} ..."
+	completion_string_1="Installation of ${app_name} complete."
+
+}
+
+set_flags () {
+
+	# Flag variables false by default
+	prerelease=false
+	purge=false
+	path=false
+
+	# Flag to note if user specified version
+	versionspec=false
+	# Flag recording if hpcflow symlink folder is on path
+	onpath=false
+
+}
+
+make_tempdir () {
+
+	# Make temp diretory and store path in a variable
+	TEMPD=$(mktemp -d)
+
+	# Exit if temp directory wasn't created successfully
+	if [ ! -r "$TEMPD" ]; then
+		echo >&2 "Failed to create temp directory for download"
+		exit 1
+	fi
+
+}
+
+parse_params () {
+
+	# Assign command line variables and flags
+	while [ $# -gt 0 ]; do
+
+		if [[ $1 == *"--"* ]]; then
+
+			param="${1/--/}"
+
+			if [[ "$param" == "prerelease" ]] || [[ $param == "purge" ]] || [[ $param == "path" ]] || [[ $param == "onefile" ]] || [[ $param == "univlink" ]]; then
+				declare $param=true
+			else
+				declare $param="$2"
+				if [[ "$param" == "version" ]]; then
+					versionspec=true
+				fi
+
+			fi
+
+			# echo $1 $2 // Optional to see the parameter:value result
+
+		fi
+
+		shift
+
+	done
+
+}
+
+if [[ "${BASH_SOURCE[0]}" ]]; then
+	run_main "$@"
 fi
-
-# Make sure temp directory is deleted on exit
-trap "exit 1"		HUP INT PIPE QUIT TERM
-trap 'rm -rf $TEMPD'	EXIT
--- test/test.bats
+++ test/test.bats
@@ -5,6 +5,24 @@ setup() {
 	_common_setup
 }
 
+teardown() {
+
+}
+
 @test "Install script runs" {
 	run install-hpcflow-flags.sh
+}
+
+@test "Installs to correct location" {
+
+	run install-hpcflow-flags.sh
+
+	if [ "$OSTYPE" == "linux-gnu"* ]; then
+
+	fi
+
+	if [ "$OSTYPE" == "darwin"* ]; then
+
+	fi
+
 }
