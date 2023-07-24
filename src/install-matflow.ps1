b91ad449dd81893b8c87285c2c8ab28cf4ecaa34
cjfullerton <chris.j.fullerton@googlemail.com>
2023-07-24 11:45:13 +0100

    test: print alias folder path

--- src/install-matflow.ps1
+++ src/install-matflow.ps1
@@ -277,6 +277,7 @@ function Create-SymLinkToApp {
 
 	if(-Not (Test-Path -PathType container $Folder\aliases))
 	{
+		Write-Host $Folder\aliases
 		New-Item -ItemType Directory -Path $Folder\aliases
 		$AliasFolderExists = Test-Path -PathType container $Folder\aliases
 		Write-Host "Alias folder exists: $AliasFolderExists"
